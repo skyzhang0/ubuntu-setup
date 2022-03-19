fcafd767d2ed9d322dde891407c18786115d7aab
skyzhang0 <qwe2213973998@163.com>
2022-03-20 01:30:56 +0800

    给基础环境配置脚本全部加了 sudo

--- setup_basic.sh
+++ setup_basic.sh
@@ -7,14 +7,14 @@
 sudo passwd root
 
 # 修改源
-mv /etc/apt/sources.list /etc/apt/sources.list.backup
-cp ./sources.list /etc/apt/sources.list
+sudo mv /etc/apt/sources.list /etc/apt/sources.list.backup
+sudo cp ./sources.list /etc/apt/sources.list
 
 # 添加 apt-key
-wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
+sudo wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
 # Fingerprint: 6084 F3CF 814B 57C1 CF12 EFD5 15CF 4D18 AF4F 7421
 
 # 更新 apt
-apt update
-apt full-upgrade -y
-apt autoremove -y
+sudo apt update
+sudo apt full-upgrade -y
+sudo apt autoremove -y
