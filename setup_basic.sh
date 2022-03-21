0a005eaacec1b39647baac41fa83054af1357730
skyzhang0 <qwe2213973998@163.com>
2022-03-21 20:57:46 +0800

    在进行基础配置的时候不再更新 apt 以最小化基础配置过程 并添加提示信息

--- setup_basic.sh
+++ setup_basic.sh
@@ -2,6 +2,7 @@
 #用于 ubuntu 系统基础配置
 
 # 获取 root 权限
+echo "Now setting root's password:"
 sudo passwd root
 
 # 修改源
@@ -12,7 +13,3 @@ sudo cp ./sources.list /etc/apt/sources.list
 sudo wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
 # Fingerprint: 6084 F3CF 814B 57C1 CF12 EFD5 15CF 4D18 AF4F 7421
 
-# 更新 apt
-sudo apt update
-sudo apt full-upgrade -y
-sudo apt autoremove -y
