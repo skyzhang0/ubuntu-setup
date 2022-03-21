77a68c3d5721bd5fb88c5d8734946e4c142886cc
skyzhang0 <qwe2213973998@163.com>
2022-03-21 20:35:55 +0800

    添加 gcc 依赖 gmp和它的依赖 m4 的安装脚本

---
+++ update_tools.d/gmp_6.2.1.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+# 编译安装 GMP
+
+# 设置 GMP 版本号，如果要安装其他版本仅需修改此变量
+gmp_version = "6.2.1"
+
+# 设置出错停止
+set -e
+
+# 设置代理
+source ../set_proxy.sh
+
+# 安装依赖
+sudo bash ./m4_1.4.19.sh
+
+if [ -f gmp-${gmp_version}.tar.xz ]; then
+    rm gmp-${gmp_version}.tar.xz
+fi
+if [ -d gmp-${gmp_version} ]; then
+    rm -r gmp-${gmp_version}
+fi
+
+# wget https://mirrors.tuna.tsinghua.edu.cn/gnu/gmp/gmp-6.2.1.tar.xz
+wget https://mirrors.tuna.tsinghua.edu.cn/gnu/gmp/gmp-${gmp_version}.tar.xz
+# tar -xf gmp-6.2.1.tar.xz
+tar -xf gmp-${gmp_version}.tar.xz
+cd gmp-${gmp_version}
+./configure
+make -j 6
+sudo make install
+cd ..
+
+if [ -f gmp-${gmp_version}.tar.xz ]; then
+    rm gmp-${gmp_version}.tar.xz
+fi
+if [ -d gmp-${gmp_version} ]; then
+    rm -r gmp-${gmp_version}
+fi
---
+++ update_tools.d/m4_1.4.19.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+# 编译安装 M4
+
+# 设置 M4 版本号，如果要安装其他版本仅需修改此变量
+m4_version = "1.4.19"
+
+# 设置出错停止
+set -e
+
+# 设置代理
+source ../set_proxy.sh
+
+if [ -f m4-${m4_version}.tar.xz ]; then
+    rm m4-${m4_version}.tar.xz
+fi
+if [ -d m4-${m4_version} ]; then
+    rm -r m4-${m4_version}
+fi
+
+# wget https://mirrors.tuna.tsinghua.edu.cn/gnu/m4/m4-1.4.19.tar.xz
+wget https://mirrors.tuna.tsinghua.edu.cn/gnu/m4/m4-${m4_version}.tar.xz
+# tar -xf m4-1.4.19.tar.xz
+tar -xf m4-${m4_version}.tar.xz
+cd m4-${m4_version}
+./configure
+make -j 6
+sudo make install 
+cd ..
+
+if [ -f m4-${m4_version}.tar.xz ]; then
+    rm m4-${m4_version}.tar.xz
+fi
+if [ -d m4-${m4_version} ]; then
+    rm -r m4-${m4_version}
+fi
