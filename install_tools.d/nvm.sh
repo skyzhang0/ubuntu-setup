8343feaf1b5f162b998eac2db5c9d59c0d70a1e0
Sky Zhang <qwe2213973998@163.com>
2022-08-17 15:11:56 +0800

    把node和包管理工具的安装脚本合并到nvm安装脚本中，并加入nodejs18的安装

--- install_tools.d/nvm.sh
+++ install_tools.d/nvm.sh
@@ -31,6 +31,23 @@ echo "[ -s \"$NVM_DIR/nvm.sh\" ] && \. \"$NVM_DIR/nvm.sh\"" | sudo tee -a /etc/p
 echo "[ -s \"$NVM_DIR/bash_completion\" ] && \. \"$NVM_DIR/bash_completion\"" | sudo tee -a /etc/profile.d/nvm.sh >sudo tee -a /etc/profile.d/nvm.sh
 echo "export NVM_NODEJS_ORG_MIRROR=https://npmmirror.com/mirrors/node/" | sudo tee -a /etc/profile.d/nvm.sh
 
+# 启用 nvm 环境
 source /etc/profile.d/nvm.sh
 
-source ${CURRENT_DIR}/npm.sh
+# 按照 node 并启用最新版
+nvm install 11
+nvm install 12
+nvm install 15
+nvm install 16
+nvm install 17
+nvm install 18
+nvm install node
+nvm use node
+
+# npm
+npm install -g cnpm --registry=https://registry.npmmirror.com
+npm install -g nrm
+nrm use taobao
+npm install -g yarn
+npm install -g yrm
+yrm use taobao
