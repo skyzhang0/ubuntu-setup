a55a22fac868f862e3d5a779730173ee1240dd61
Sky Zhang <qwe2213973998@163.com>
2022-08-17 18:12:04 +0800

    cpython安装脚本可以接收第一个参数为版本号

--- update_tools.d/cpython.sh
+++ update_tools.d/cpython.sh
@@ -2,7 +2,10 @@
 # 编译安装 python 最新版本
 
 # 设置 CMake 版本号，如果要安装其他版本仅需修改此变量
-python_version="3.9.12"
+python_version="3.9.13"
+if [ $# -ge 1 ]; then # 若有参数则把第一个作为版本号
+    python_version="$1"
+fi
 
 # 设置出错停止
 set -e
@@ -10,12 +13,16 @@ set -e
 # 设置代理
 source ../set_proxy.sh
 
+# 安装依赖
+apt install -y libgdbm-dev
+apt install -y readline-common readline-doc libreadline-dev
+
 [ -d ./cpython/ ] && rm -r ./cpython/
 
 git clone https://gitee.com/mirrors/cpython.git cpython
 
 cd cpython
-# git checkout v3.9.12
+# git checkout vx.x.x
 git checkout v${python_version}
 ./configure --enable-optimizations
 make -j 8
