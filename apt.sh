0b2202cf1be11600f6df074a72f43261ecaf0946
Sky Zhang <qwe2213973998@163.com>
2022-08-17 18:12:30 +0800

    添加了几个常用工具及其介绍

--- apt.sh
+++ apt.sh
@@ -9,14 +9,20 @@ apt update -y
 apt full-upgrade -y
 
 # 常用工具
-apt install -y dpkg git wget curl gnupg ca-certificates openssl subversion tree
+apt install -y dpkg git wget curl gnupg openssl subversion
+apt install -y ca-certificates # CA证书 用于ssl等
+apt install -y tree # 树状输出子目录
+apt install -y sqlite3 libsqlite3-dev # 轻量级数据库
+apt install -y bzip2 bzip2-doc libbz2-1.0 libbz2-dev # 压缩工具+1
+apt install -y uuid uuid-dev libuuid1 # 通用唯一识别码工具
+
 
 # C++ 开发工具
 apt install -y cmake valgrind
-# 远程 Clion 开发
+# 配置远程 Clion 开发
 bash clion_setup_env.sh
 # GCC 工具链
-apt install -y build-essential gcc gdb 
+apt install -y build-essential gcc gdb
 # LLVM 工具链（development branch） see:https://apt.llvm.org/
 apt install -y libllvm-15-ocaml-dev libllvm15 llvm-15 llvm-15-dev llvm-15-doc llvm-15-examples llvm-15-runtime
 apt install -y clang-15 clang-tools-15 clang-15-doc libclang-common-15-dev libclang-15-dev libclang1-15 clang-format-15 python3-clang-15 clangd-15 clang-tidy-15
@@ -41,6 +47,7 @@ apt install -y python3.8-dev python3.9-dev python3-pip
 # ln -s /usr/bin/python3.9 /usr/bin/python3
 # [ -f /usr/bin/python ] && rm /usr/bin/python
 # ln -s /usr/bin/python3 /usr/bin/python
+# pip 换源
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pip -U
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 
