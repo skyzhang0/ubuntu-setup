9377d2cfb5e2aa01ce0d55727462d0f6c1c2d316
Sky Zhang <qwe2213973998@163.com>
2022-08-18 17:13:52 +0800

    修复历史遗留问题

--- update_tools.d/gcc.sh
+++ update_tools.d/gcc.sh
@@ -2,7 +2,7 @@
 # 编译安装 GCC 工具链最新版本
 
 # 设置 GCC/GDB 默认版本号
-gcc_version="11.2"
+gcc_version="12.1.0"
 if [ $# -ge 1 ]; then # 若有参数则把第一个作为版本号
     gcc_version="$1"
 fi
@@ -18,14 +18,14 @@ sudo bash ./gmp_6.2.1.sh
 sudo bash ./mpfr_4.1.0.sh
 sudo bash ./mpc_1.2.1.sh
 
-if [ -f gcc-${gcc_version}.0.tar.gz ]; then
-    rm gcc-${gcc_version}.0.tar.gz
+if [ -f gcc-${gcc_version}.tar.gz ]; then
+    rm gcc-${gcc_version}.tar.gz
 fi
 if [ -f gdb-${gcc_version}.tar.gz ]; then
     rm gdb-${gcc_version}.tar.gz
 fi
-if [ -d gcc-${gcc_version}.0 ]; then
-    rm -r gcc-${gcc_version}.0
+if [ -d gcc-${gcc_version} ]; then
+    rm -r gcc-${gcc_version}
 fi
 if [ -d gdb-${gcc_version} ]; then
     rm -r gdb-${gcc_version}
@@ -34,14 +34,14 @@ fi
 # 下载并解压源码
 # wget https://mirrors.cloud.tencent.com/gnu/gcc/gcc-11.2.0/gcc-11.2.0.tar.gz
 # wget https://mirrors.cloud.tencent.com/gnu/gdb/gdb-11.2.tar.gz
-wget https://mirrors.cloud.tencent.com/gnu/gcc/gcc-${gcc_version}.0/gcc-${gcc_version}.0.tar.gz
-wget https://mirrors.cloud.tencent.com/gnu/gnu/gdb/gdb-${gcc_version}.tar.gz
+wget https://mirrors.cloud.tencent.com/gnu/gcc/gcc-${gcc_version}/gcc-${gcc_version}.tar.gz
+wget https://mirrors.cloud.tencent.com/gnu/gdb/gdb-${gcc_version}.tar.gz
 # tar -zxf ./gcc-11.2.0.tar.gz
 # tar -zxf ./gdb-11.2.tar.gz
-tar -zxf ./gcc-${gcc_version}.0.tar.gz
+tar -zxf ./gcc-${gcc_version}.tar.gz
 tar -zxf ./gdb-${gcc_version}.tar.gz
 
-cd gcc-${gcc_version}.0
+cd gcc-${gcc_version}
 ./configure --disable-multilib
 make -j 6
 sudo make install
@@ -56,14 +56,14 @@ cd ..
 rm /usr/lib/x86_64-linux-gnu/libstdc++.so.6
 ln -s /usr/local/lib64/libstdc++.so.6 /usr/lib/x86_64-linux-gnu/libstdc++.so.6
 
-if [ -f gcc-${gcc_version}.0.tar.gz ]; then
-    rm gcc-${gcc_version}.0.tar.gz
+if [ -f gcc-${gcc_version}.tar.gz ]; then
+    rm gcc-${gcc_version}.tar.gz
 fi
 
 if [ -f gdb-${gcc_version}.tar.gz ]; then
     rm gdb-${gcc_version}.tar.gz
 fi
-if [ -d gcc-${gcc_version}.0 ]; then
+if [ -d gcc-${gcc_version} ]; then
     rm -r gcc-${gcc_version}.0
 fi
 if [ -d gdb-${gcc_version} ]; then
