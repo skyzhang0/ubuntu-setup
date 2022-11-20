6b489163dd34fb9559daf4a62491f52a7a1c8963
Sky Zhang <qwe2213973998@163.com>
2022-11-20 22:30:21 +0800

    now use default version in subscript.

--- update.sh
+++ update.sh
@@ -6,13 +6,13 @@ set -x # 设置输出执行的命令
 
 cd ./update_tools.d
 
-# 更新 cmake 到 3.21
-bash ./cmake.sh 3.21.6
+# 更新 cmake
+bash ./cmake.sh
 
-# 更新 gcc 到 11.2.0
-bash ./gcc.sh 11.2.0
+# 更新 gcc
+bash ./gcc.sh
 
-# 更新 python 到 3.9.13
-bash ./cpython.sh 3.9.13
+# 更新 python
+bash ./cpython.sh
 
 cd ..
